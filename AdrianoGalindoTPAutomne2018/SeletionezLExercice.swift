51aed61827b9302664007a22f6673f99f9f62417
ASPOL-PE <adrianogalindo@gmail.com>
2018-12-06 19:31:13 -0500

    Coded the "selectionez exercice" storyboard

--- AdrianoGalindoTPAutomne2018/SeletionezLExercice.swift
+++ AdrianoGalindoTPAutomne2018/SeletionezLExercice.swift
@@ -11,8 +11,6 @@ import UIKit
 
 class SeletionezLExercice: UIViewController {
     
-
-    
     @IBOutlet weak var nbSeriesLabelInput: UILabel!
     @IBOutlet weak var nbSeriesTextFieldInput: UITextField!
     
@@ -27,10 +25,38 @@ class SeletionezLExercice: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //NbSeriesInput.text
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        resetTextFields()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dissmissKeyboard))
+        
+        self.view.addGestureRecognizer(tapGesture)
     }
     
+    @objc func dissmissKeyboard() {
+        self.view.endEditing(true)
+    }
     
+    func resetTextFields() {
+        
+        nbSeriesTextFieldInput.text = "0"
+        nbSeriesTextFieldInput.delegate = self
+        
+        nbRepetitionsTextFieldInput.text = "0"
+        nbRepetitionsTextFieldInput.delegate = self
+        
+        poidsTextFieldInput.text = "0"
+        poidsTextFieldInput.delegate = self
+        
+        tempsSeanceTextFieldInput.text = "0"
+        tempsSeanceTextFieldInput.delegate = self
+    }
+}
+extension SeletionezLExercice: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
